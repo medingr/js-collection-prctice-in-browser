ea9ae478450f2f6fbe817629a6dd5dffe4437c78
medingr <32764049+medingr@users.noreply.github.com>
2019-09-15 21:00:52 +0800

    Update practice-2.js

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,11 @@
 'use strict';
 
 function countSameElements(collection) {
-  return 'Implement the practice require, and begin changing code in this row';
+  const resultSet = new Array;
+  Array.from(new Set(collection))
+  .map(row => resultSet.push({ 
+          key: row.includes("-")   ?  row.split("-")[0]             : row, 
+          count: row.includes("-") ?  parseInt(row.split("-")[1])   : collection.filter(y => y === row).length })
+  );
+  return resultSet;
 }
