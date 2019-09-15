776b5c9e9dad8728ebaedfbad1b135d2d281270c
medingr <32764049+medingr@users.noreply.github.com>
2019-09-15 21:04:50 +0800

    Update practice-2.js

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,5 +1,23 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+    collectionA.forEach(collectA => {
+        objectB.value.forEach(objB => {
+
+            let indexOfObjectB = objectB.value.findIndex(element => element === objB);
+            let originalCount = collectA.count;
+            if( collectA.key == objB && originalCount == 3  )
+            { 
+                collectA.count -- ; 
+            }
+            else if 
+            ( collectA.key == objB ) 
+            { 
+                collectA.count = originalCount - indexOfObjectB;
+            }
+            else
+                collectA.count; 
+        });
+    });
+    return collectionA;
 }
