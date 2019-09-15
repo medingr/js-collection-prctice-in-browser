d8ba129d08308ce22ea6b3326d87dc6b2f21ed9f
medingr <32764049+medingr@users.noreply.github.com>
2019-09-15 20:56:22 +0800

    Update practice-1.js

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,7 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  // return 'Implement the practice require, and begin changing code in this row';
+  const SameElements = collectionA.filter( n => collectionB.includes(n) );
+  return SameElements;
 }
