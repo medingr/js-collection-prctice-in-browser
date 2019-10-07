04c4e995acca394c3997eb571760a4a0553c289d
medingr <32764049+medingr@users.noreply.github.com>
2019-10-07 20:42:20 +0800

    update pratice 3 and 4

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,5 +1,31 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+
+    const resultSet = new Array;
+    [...new Set(collectionA)]
+    .map(
+        letter => resultSet.push({ key: letter, count: collectionA.filter(y => y === letter).length })
+    );
+
+    resultSet.forEach(collectA => {
+    objectB.value.forEach(objB => {
+  
+        let indexOfObjectB = objectB.value.findIndex(element => element === objB);
+        let originalCount = collectA.count;
+        if( collectA.key == objB && originalCount == 3  )
+        { 
+            collectA.count -- ; 
+        }
+        else if 
+        ( collectA.key == objB ) 
+        { 
+            collectA.count = originalCount - indexOfObjectB;
+        }
+        else
+            collectA.count; 
+
+    });
+});
+return resultSet;
 }
--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -1,5 +1,32 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+
+  const resultSet = new Array;
+  Array.from(new Set(collectionA))
+  .map(row => resultSet.push({ 
+          key: row.includes("-")   ?  row.split("-")[0]             : row, 
+          count: row.includes("-") ?  parseInt(row.split("-")[1])   : collectionA.filter(y => y === row).length })
+  );
+
+  resultSet.forEach(collectA => {
+    objectB.value.forEach(objB => {
+  
+        let indexOfObjectB = objectB.value.findIndex(element => element === objB);
+        let originalCount = collectA.count;
+        if( collectA.key == objB && originalCount == 3  )
+        { 
+            collectA.count -- ; 
+        }
+        else if 
+        ( collectA.key == objB ) 
+        { 
+            collectA.count = originalCount - indexOfObjectB;
+        }
+        else
+            collectA.count; 
+
+    });
+});
+return resultSet;
 }
